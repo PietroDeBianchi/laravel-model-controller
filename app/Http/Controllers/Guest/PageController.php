b4311b7c5640c971bb67f9616bd3acaee9f26dc9
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-05-11 18:06:23 +0200

    connected to db_movies :)

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
+        // take all the movies from db_movies in App/Models/Movie
+        $movies = Movie::all();
+        dd($movies);
         // push the return to from web.php
         return view('home');
     }
