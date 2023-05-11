41467e056f546ac30fac5a252ef10426190b4836
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-05-11 18:35:35 +0200

    added some style :)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,18 @@
 
 {{-- chaghe the 'contents' of app.blade (also html elements) --}}
 @section('contents')
-    <div id="home-container">
-        <ul>
-            @foreach ($movies as $movie)
-                <li>{{$movie->title}}</li>
-            @endforeach
-        </ul>
+    <div id="home-container" class="d-flex flex-wrap justify-content-between">
+        @foreach ($movies as $movie)
+        <div class="card mt-3">
+            <div class="card-body p-1 text-center">
+                <h5 class="card-title">{{$movie->title}}</h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary">{{$movie->nationality}}</h6>
+                <span href="#" class="card-link">{{$movie->date}}</span>
+                <div>
+                    <span href="#" class="card-link">Rating from users : {{$movie->vote}}</span>
+                </div>
+            </div>
+        </div>
+    @endforeach
     </div>
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
-<div>
-    <h1>FEETFOOT</h1>
+<div class="container">
+    <p class="text-center">Your one-stop-shop for streaming the latest movies and TV shows in HD. Get personalized recommendations, multi-language options, and more. Sign up for our subscription-based service or try it out with our free trial today!</p>
 </div>
